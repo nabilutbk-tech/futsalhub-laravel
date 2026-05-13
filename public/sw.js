b84e77e8d4dfc6bba9a65959084b76227046ed10
nabilutbk-tech <nabilutbk@gmail.com>
2026-05-13 08:53:31 +0700

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (event) => {
-    // Biarkan kosong untuk PWA basic. 
-    // Ke depannya bisa diisi kode untuk mode Offline.
-});
